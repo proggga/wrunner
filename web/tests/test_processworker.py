9b41ae53af2ddda8ff0a8f9015470ad0502943c8
Mikhail Fesenko <proggga@gmail.com>
2017-04-25 15:29:54 +0300

    Covered to 100%

---
+++ web/tests/test_processworker.py
@@ -0,0 +1,25 @@
+'''test for subprocess'''
+import os
+import re
+from web.worker import ProcessWorker
+
+
+# Create your tests here.
+def test_execute_command():
+    '''simple test with long command'''
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'web.settings'
+    command = 'echo "This is first line"; sleep 1; '\
+              'echo "but this is next line"; sleep 3; '\
+              'echo "and the end of text"; sleep 3;'\
+              'echo -n "done\nhello"'
+    pworker = ProcessWorker()
+    should_lines = 'This is first line\n'\
+                   'but this is next line\n'\
+                   'and the end of text\n'\
+                   'done\n'\
+                   'hello'
+    should_lines_array = [re.sub(r'\|$', '\n', line) for line in should_lines
+                          .replace('\n', '|Q|').split('Q|') if line]
+    for line in pworker.execute(command):
+        assert line == should_lines_array.pop(0)
+    assert pworker.result_lines == should_lines
