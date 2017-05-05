ab82ebc6a62a478a0fc8a56c5f26c9e8c821c902
Mikhail Fesenko <proggga@gmail.com>
2017-05-05 23:55:12 +0300

    changed celery task test

--- web/tests/test_task_exec.py
+++ web/tests/test_task_exec.py
@@ -1,29 +1,25 @@
 '''test task with exec'''
 from django.test import override_settings
-import mock as mock
-import pytest
+from mock import patch
+from mock import call
 
 from web.tasks import execute_command_task
 
 
-@mock.patch('web.tasks.execute_command_task.update_state')
+@patch('web.tasks.execute_command_task.update_state')
 @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
-@pytest.mark.django_db
 def test_create_task(update_state_method):
     '''test task created and return progress'''
-    command = 'echo "start"; sleep 0.1; '\
-              'echo "message"; sleep 0.1; '\
-              'echo -n "end"'
-    task = execute_command_task.delay(command)
+    command = 'example command'
+    with patch('web.tasks.ProcessWorker.execute') as execute_method:
+        execute_method.return_value = ['line1', 'line2']
+        task = execute_command_task.delay(command)
     calls = []
-    result = ''
-    result += 'started command: {}\n'.format(command)
-    calls.append(mock.call(state='PROGRESS', meta={'stdout': result}))
-    result += 'start\n'
-    calls.append(mock.call(state='PROGRESS', meta={'stdout': result}))
-    result += 'message\n'
-    calls.append(mock.call(state='PROGRESS', meta={'stdout': result}))
-    result += 'end\n'
-    calls.append(mock.call(state='PROGRESS', meta={'stdout': result}))
+    result = 'started command: {}\n'.format(command)
+    calls.append(call(state='PROGRESS', meta={'stdout': result}))
+    result = 'started command: {}\nline1\n'.format(command)
+    calls.append(call(state='PROGRESS', meta={'stdout': result}))
+    result = 'started command: {}\nline1\nline2\n'.format(command)
+    calls.append(call(state='PROGRESS', meta={'stdout': result}))
     update_state_method.assert_has_calls(calls)
     assert task.status == 'SUCCESS'
